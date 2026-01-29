7e4e1ca327580e999c27978a94558e3b742d0fe2
Mahima Chaudhary <chaudharymahima894@gmail.com>
2026-01-29 21:41:08 +0530

    Clean repo setup

---
+++ frontend/src/components/OurPolicy.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const OurPolicy = () => {
+  return (
+    <div className='flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-20 text-xs sm:text-sm md:text-base rgb(55,65,81)text-gray-700'>
+      
+      <div>
+        <img src={assets.exchange_icon} className='w-12 m-auto mb-5' alt=""/>
+     <p className='font-semibold'> Easy Exchange Policy </p>
+     <p className='text-gray-400'> We offer hassle free exchange policy</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default OurPolicy
---
+++ frontend/src/context/ShopContext.jsx
@@ -0,0 +1,20 @@
+import { createContext } from "react";
+import { products } from "../assets/assets"
+
+export const ShopContext = createContext();
+
+const ShopContextProvider = (props) => {
+    const currency='₹';
+    const deliver_fee=10;
+    
+    const value = {
+        products, currency, deliver_fee
+    }
+
+    return (
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+export default ShopContextProvider;
