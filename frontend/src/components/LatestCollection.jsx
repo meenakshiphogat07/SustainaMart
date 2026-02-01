25523539c0f6788ab76f50fe205c8229ae9fb8ad
meenakshiphogat07 <meenakshiphogat07@gmail.com>
2026-02-01 14:12:54 +0530

    changed logo

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -15,7 +15,7 @@ const LatestCollection = () => {
         <div className='my-10'>
             <div className='text-center py-8 text-3xl'>
                 <Title text1={'LATEST'} text2={'COLLECTIONS'} />
-                <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia quod in id. Dolorum inventore similique iste rerum corrupti enim cumque autem. Ea repellendus ex voluptatem harum aliquam perferendis, nemo voluptatibus?</p>
+                <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600 '>Discover our newest eco-friendly essentials, thoughtfully crafted from sustainable materials — because caring for the planet never goes out of style.</p>
 
             </div>
             <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const {setShowSearch}=useContext(ShopContext);
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
-      <img src={assets.logo} className='w-36' alt="SustainaMart Logo" />
+      <img src={assets.logo} className='w-60 scale-125' alt="SustainaMart Logo" />
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
         <NavLink to='/' className='flex flex-col items-center gap-1'>
           <p>HOME</p>
