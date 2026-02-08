3650d3b1df3b3344e61565f88da426cbe4679b6e
Nitika <nitikaruhela.01@gmail.com>
2026-02-08 15:23:05 +0530

    created adminAuth and changes in userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -91,7 +91,21 @@ const registerUser = async (req, res) => {
 
 //Route for admin login
 const adminlogin = async (req, res) => {
+try{
+    const{email,password} = req.body
 
+    if (email === Process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+        const token = jwt.sign(email+password,process.env.JWT_SECRET);
+        res.json({success:true,token})
+    }else {
+        res.json({success:false,message:"INVALID CREDENTIALS"})
+    }
+}
+
+catch(error){
+    console.log(error);
+    res.json({success:false,message: error.message})
+}
 }
 
 
